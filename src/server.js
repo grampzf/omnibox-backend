b0728990739bea2ee98dae2e772457c21533164a
Greyson Rampazo <greysonf@gmail.com>
2019-04-10 15:45:49 -0300

    Adiciona Variaveis Ambiente

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
+const cors = require('cors')
 
 const app = express()
+
+app.use(cors())
 
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
@@ -30,4 +33,4 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')))
 
 app.use(require('./routes'))
 
-server.listen(3030, () => console.log('Backend is running...'))
+server.listen(process.event.PORT || 3030)
